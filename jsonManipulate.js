8ebb6f7579d3748ec95a0f71f08f8cf6439725d1
unknown <swenee.hagberg@gmail.com>
2021-04-01 10:36:13 +0200

    nowConumesJsonInput

--- jsonManipulate.js
+++ jsonManipulate.js
@@ -2,9 +2,37 @@
 
 console.log("application start: ---------");
 
-A = ["a"];
-funk(A);
+let racesList = [];
 
-function funk(A) {
 
+consumeHorseRacing();
+//getUniqeCourses();
+
+async function consumeHorseRacing() {
+
+  var axios = require('axios');
+
+  const resp = await axios.get("https://bricco.se/goop.json");
+
+
+  resp.data.forEach((item, i) => {
+    const oneRace = JSON.parse(JSON.stringify(item));
+    racesList.push(oneRace);
+  });
+
+  return racesList;
+}
+
+
+function getUniqeCourses(){
+//  console.log(racesList[0]);
+/*
+  var unique = racesList.filter(
+    function (value, index, self) {
+      return self.indexOf(value) === index;
+    }
+  );
+  */
+      //unika banor
+      //sort i bokstavsordning
 }
